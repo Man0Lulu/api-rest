061a9964a32566f864ac84d8b8dcbfeeaad1434f
Luís Nascimento de Sousa <luisrocksousa1111@gmail.com>
2024-04-23 20:14:37 -0300

    instalando o nodemon e colocando parametros

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/sobre', function(req, res){
     res.send('sobre mim');
 });
 
+app.get('/sobre/:id', function(req, res){
+    res.send("oi " + req.params.id);
+});
+
+app.get('/sobre/:id/:conteudo', function(req, res){
+    res.send(req.params);
+});
+
 app.listen(8081, function(req, res){
     console.log('servidor rodando na porta 8081');
 });
